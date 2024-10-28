0c173aadbe57b056fe61e13f30ca277719672c03
Ubuntu <ubuntu@ip-172-31-36-167.ec2.internal>
2024-10-28 21:08:11 +0000

    changed max_length for StripeModel

--- backend/account/models.py
+++ backend/account/models.py
@@ -8,7 +8,7 @@ class StripeModel(models.Model):
     email = models.EmailField(null=True, blank=True)
     name_on_card = models.CharField(max_length=200, null=True, blank=True)
     customer_id = models.CharField(max_length=200, blank=True, null=True)
-    card_number = models.CharField(max_length=16, unique=True, null=True, blank=True)
+    card_number = models.CharField(max_length=20, unique=True, null=True, blank=True)
     exp_month = models.CharField(max_length=2, validators=[RegexValidator(r'^\d{0,9}$')], null=True, blank=True)
     exp_year = models.CharField(max_length=4, validators=[RegexValidator(r'^\d{0,9}$')], null=True, blank=True)
     card_id = models.TextField(max_length=100, null=True, blank=True)
@@ -16,7 +16,7 @@ class StripeModel(models.Model):
     address_city = models.CharField(max_length=120, null=True, blank=True)
     address_country = models.CharField(max_length=120, null=True, blank=True)
     address_state = models.CharField(max_length=120, null=True, blank=True)
-    address_zip = models.CharField(max_length=6, validators=[RegexValidator(r'^\d{0,9}$')], null=True, blank=True)
+    address_zip = models.CharField(max_length=10, validators=[RegexValidator(r'^\d{0,9}$')], null=True, blank=True)
 
     def __str__(self):
         return self.email
